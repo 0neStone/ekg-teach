c21f60c229ca01fb8dc1f00a9581e7f1ea68142b
frederikseeba <frederik.seeba@gmail.com>
2019-01-07 21:20:08 +0100

    Added levels, first beispiellabyrinth

--- background.py
+++ background.py
@@ -1,11 +1,11 @@
 import turtle
 import time
 
-class labyrinth():
+class labyrinth:
     def __init__(self):
         self.drawer = turtle.Turtle()
         self.drawer.hideturtle()
-        self.drawer.speed(10**10)
+        turtle.tracer(0, 0)
 
         self.drawer.up()
         self.x0 = -200
@@ -27,6 +27,7 @@ class labyrinth():
             self.end(self.x0 + x*self.stepwidth, self.y0 + y*self.stepwidth, "ziel")
         else:
             print("Dieses item gibt es nicht!")
+        turtle.update()
 
     def stein(self, x ,y, xx, yy):
         self.drawer.up()
@@ -95,51 +96,93 @@ class labyrinth():
             self.drawer.write(y, font=("Arial", 10))
         self.drawer.up()
 
-class player():
-    def __init__(self):
+class player:
+    def __init__(self, x0, y0, x1, y1):
         self.schildie = turtle.Turtle()
         self.schildie.shape("turtle")
         self.schildie.up()
-        self.schildie.goto(25, 25)
+        self.geheZu(x0, y0)
         self.x0 = -200
         self.y0 = -200
+        self.endX = x1
+        self.endY = y1
         self.stepwidth = 50
         self.richtung = 1
+        self.sleeptime = 1
+
+    def geheZu(self, x, y):
+        x *= laby.stepwidth
+        y *= laby.stepwidth
+        x += laby.stepwidth/2
+        y += laby.stepwidth/2
+        x += laby.x0
+        y += laby.y0
+        self.schildie.goto(x, y)
 
     def vor(self, steps):
-        if self.vorne():
+        print(self.vorne("vor"))
+        time.sleep(self.sleeptime)
+        if self.vorne("vor"):
             print("Es liegt ein Stein vor dir, du kannst da nicht hingehen")
         else:
             self.schildie.forward(self.stepwidth*steps)
+            turtle.update()
+        if self.getposition() == [self.endX, self.endY]:
+            print("Du hast es geschafft!")
 
     def zurueck(self, steps):
-        self.schildie.backward(self.stepwidth*steps)
+        time.sleep(self.sleeptime)
+        if self.vorne("rück"):
+            print("Es liegt ein Stein hinter dir, du kannst da nicht hingehen")
+        else:
+            self.schildie.backward(self.stepwidth*steps)
+            turtle.update()
 
     def links(self):
+        time.sleep(self.sleeptime)
         self.schildie.left(90)
         self.richtung = self.aenderausrichtung("left", self.richtung)
+        turtle.update()
 
     def rechts(self):
+        time.sleep(self.sleeptime)
         self.schildie.right(90)
         self.richtung = self.aenderausrichtung("right", self.richtung)
+        turtle.update()
 
-    def vorne(self):
+    def vorne(self, dir):
+        time.sleep(self.sleeptime)
         eigenfeld = self.getposition()
         eigenX = eigenfeld[0]
         eigenY = eigenfeld[1]
 
-        if self.richtung == 0: #oben
-            vorderX = eigenX
-            vorderY = eigenY + 1
-        elif self.richtung == 1: #rechts
-            vorderX = eigenX +1
-            vorderY = eigenY
-        elif self.richtung == 2: # unten
-            vorderX = eigenX
-            vorderY = eigenY - 1
-        elif self.richtung == 3: # links
-            vorderX = eigenX - 1
-            vorderY = eigenY
+        if dir == "vor":
+            if self.richtung == 0: #oben
+                vorderX = eigenX
+                vorderY = eigenY + 1
+            elif self.richtung == 1: #rechts
+                vorderX = eigenX +1
+                vorderY = eigenY
+            elif self.richtung == 2: # unten
+                vorderX = eigenX
+                vorderY = eigenY - 1
+            elif self.richtung == 3: # links
+                vorderX = eigenX - 1
+                vorderY = eigenY
+        else:
+            if self.richtung == 0: #oben
+                vorderX = eigenX
+                vorderY = eigenY - 1
+            elif self.richtung == 1: #rechts
+                vorderX = eigenX - 1
+                vorderY = eigenY
+            elif self.richtung == 2: # unten
+                vorderX = eigenX
+                vorderY = eigenY + 1
+            elif self.richtung == 3: # links
+                vorderX = eigenX +1
+                vorderY = eigenY
+
         vorderCoords = [vorderX, vorderY]
         return self.besetzt(vorderX, vorderY)
 
@@ -174,5 +217,27 @@ class player():
             richtung = 3
         return richtung
 
+def beispiellabyrinth(id):
+    if id==1:
+        for x in range(0, laby.borderlength):
+            laby.setze("Stein", x, 0)
+            laby.setze("Stein", x, laby.borderlength-1)
+        for y in range(0, laby.borderlength):
+            laby.setze("Stein", 0, y)
+            laby.setze("Stein", laby.borderlength-1, y)
+        laby.setze("Ziel", endX, endY)
+        laby.setze("Start", startX, startY)
+        for x in range(0, laby.borderlength-2):
+            laby.setze("Stein", x, 2)
+        for x in range(2, laby.borderlength):
+            laby.setze("Stein", x, 4)
+        for x in range(0, laby.borderlength-2):
+            laby.setze("Stein", x, 6)
+        for x in range(2, laby.borderlength):
+            laby.setze("Stein", x, 8)
+startX = 2
+startY = 1
+endX = 1
+endY = 8
 laby = labyrinth()
-spieler = player()
+spieler = player(startX, startY, endX, endY)
--- foreground.py
+++ foreground.py
@@ -1,15 +1,8 @@
 from background import *
 
-def labyrinth():
-    for x in range(0, 5):
-        for y in range(0, 5):
-            pass
-            laby.setze("Stein", x*2+1, y*2+1)
-    laby.setze("Ziel", 3, 3)
-    laby.setze("Start", 5, 8)
-labyrinth()
+beispiellabyrinth(1)
 spieler.vor(1)
 spieler.links()
 spieler.vor(1)
-print(spieler.vorne())
+print(spieler.vorne("vor"))
 input()
