ee26e8753c03c7715756a7fb57aa82b13826df96
frederikseeba <frederik.seeba@gmail.com>
2019-01-07 16:25:43 +0100

    Added background class, border and gitter

--- background.py
+++ background.py
@@ -1,81 +1,119 @@
 import turtle
 import time
 
-schildkröte = turtle.Turtle()
-schildkröte.shape("turtle")
-schildkröte.up()
+class background():
+    def __init__(self):
+        self.schildie = turtle.Turtle()
+        self.schildie.shape("turtle")
+        self.schildie.up()
 
-drawer = turtle.Turtle()
-drawer.hideturtle()
+        self.drawer = turtle.Turtle()
+        self.drawer.hideturtle()
+        self.drawer.speed(100)
 
-def vor(steps):
-  schildkröte.forward(50*steps)
+        self.drawer.up()
+        self.x0 = -200
+        self.y0 = -200
+        self.stepwidth = 50
+        self.borderlength = 10
+        self.border()
+        self.gitter()
 
-def zurück(steps):
-    schildkröte.backward(50*steps)
+    def vor(self, steps):
+      self.schildie.forward(self.stepwidth*steps)
 
-def links():
-    schildkröte.left(90)
+    def zurück(self, steps):
+        self.schildie.backward(self.stepwidth*steps)
 
-def rechts():
-    schildkröte.right(90)
+    def links(self):
+        self.schildie.left(90)
 
-def vorne():
-    vor = True
-    return True
+    def rechts(self):
+        self.schildie.right(90)
 
-def setze(item):
-    x  = schildkröte.xcor()
-    y = schildkröte.ycor()
-    if item == "Stein":
-        stein(x, y)
-    elif item == "Start":
-        start(x, y)
-    elif item == "Ziel":
-        ziel(x, y)
-    else:
-        print("Dieses item gibt es nicht!")
+    def vorne(self):
+        vor = True
+        return True
 
-def stein(x ,y):
-    drawer.up()
-    drawer.goto(x, y)
-    drawer.down()
-    drawer.begin_fill()
-    drawer.color("grey")
-    for x in range(0, 4):
-        drawer.forward(50)
-        drawer.left(90)
-    drawer.end_fill()
-    drawer.up()
+    def setze(self, item):
+        x  = self.schildie.xcor()
+        y = self.schildie.ycor()
+        if item == "Stein":
+            self.stein(x, y)
+        elif item == "Start":
+            self.start(x, y)
+        elif item == "Ziel":
+            self.ziel(x, y)
+        else:
+            print("Dieses item gibt es nicht!")
 
-def start(x ,y):
-    drawer.up()
-    drawer.goto(x, y)
-    drawer.down()
-    drawer.begin_fill()
-    drawer.color("red")
-    for x in range(0, 4):
-        drawer.forward(50)
-        drawer.left(90)
-    drawer.end_fill()
-    drawer.up()
+    def stein(self, x ,y):
+        self.drawer.up()
+        self.drawer.goto(x, y)
+        self.drawer.down()
+        self.drawer.begin_fill()
+        self.drawer.color("grey")
+        for x in range(0, 4):
+            self.drawer.forward(self.stepwidth)
+            self.drawer.left(90)
+        self.drawer.end_fill()
+        self.drawer.up()
 
-def ziel(x ,y):
-    drawer.up()
-    drawer.goto(x, y)
-    drawer.down()
-    drawer.begin_fill()
-    drawer.color("gold")
-    for x in range(0, 4):
-        drawer.forward(50)
-        drawer.left(90)
-    drawer.end_fill()
-    drawer.up()
+    def start(self, x ,y):
+        self.drawer.goto(x, y)
+        self.drawer.down()
+        self.drawer.begin_fill()
+        self.drawer.color("red")
+        for x in range(0, 3):
+            self.drawer.forward(self.stepwidth)
+            self.drawer.left(120)
+        self.drawer.end_fill()
+        self.drawer.up()
 
-setze("Stein")
-vor(3)
-setze("Ziel")
-links()
-vor(3)
-setze("Start")
+    def ziel(self, x ,y):
+        self.drawer.goto(x, y)
+        self.drawer.down()
+        self.drawer.begin_fill()
+        self.drawer.color("gold")
+        for x in range(0, 3):
+            self.drawer.forward(self.stepwidth)
+            self.drawer.left(120)
+        self.drawer.end_fill()
+        self.drawer.up()
+
+    def border(self):
+        self.drawer.goto(self.x0, self.y0)
+        self.drawer.down()
+        self.drawer.color("black")
+        for x in range(0, 4):
+            self.drawer.forward(self.borderlength*self.stepwidth)
+            self.drawer.left(90)
+        self.drawer.up()
+
+    def gitter(self):
+        self.drawer.goto(self.x0, self.y0)
+        self.drawer.color("#666666")
+        self.drawer.left(90)
+        for x in range(0, self.borderlength):
+            self.drawer.up()
+            self.drawer.goto(x*self.stepwidth, 0)
+            self.drawer.down()
+            self.drawer.forward(self.borderlength*self.stepwidth)
+        self.drawer.right(90)
+        for y in range(0, self.borderlength):
+            self.drawer.up()
+            self.drawer.goto(0, y*self.stepwidth)
+            self.drawer.down()
+            self.drawer.forward(self.borderlength*self.stepwidth)
+        self.drawer.up()
+
+
+
+b = background()
+b.setze("Stein")
+b.vor(3)
+b.setze("Ziel")
+b.links()
+b.vor(3)
+b.setze("Start")
 input()
